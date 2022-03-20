299f826aa81533429299bb7c26a78cf39fe9e695
Robert Trostle <51966615+robtrostle@users.noreply.github.com>
2022-03-20 12:00:52 -0400

    Adding search bar

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   date: Date = new Date();
+
+  text: string = "abc";
+
+  
   constructor() { }
 
   ngOnInit(): void {
