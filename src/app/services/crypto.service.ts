86a7acd1ad410416f530205ad3dbd7de5c431fd5
Christian Sifuentes <me@radixpoint.dev>
2022-04-13 00:52:21 -0400

    hotfix(): adding back constructor method

--- src/app/services/crypto.service.ts
+++ src/app/services/crypto.service.ts
@@ -6,5 +6,6 @@ import { environment } from 'src/environments/environment';
 })
 export class CryptoService {
   static readonly BASE_API_URL: string = `${environment.mockBaseUrl}/cryptowatcher`;
-
+  constructor() { }
+  
 }
