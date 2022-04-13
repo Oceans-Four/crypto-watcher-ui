dcbf8285a2a0cc4e4af7432aa3e0ef04182a0d01
Christian Sifuentes <me@radixpoint.dev>
2022-04-13 00:49:32 -0400

    Adding environments for mock and localhost

--- src/app/services/crypto.service.ts
+++ src/app/services/crypto.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CryptoService {
+  static readonly BASE_API_URL: string = `${environment.mockBaseUrl}/cryptowatcher`;
 
-  constructor() { }
 }
