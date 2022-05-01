cbd272830888804db94de55b5adcfcd9630a9313
Christian Sifuentes <me@radixpoint.dev>
2022-05-01 11:45:41 -0400

    Adding Breaking News component

---
+++ src/app/pages/breaking-news-strip/breaking-news-strip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breaking-news-strip',
+  templateUrl: './breaking-news-strip.component.html',
+  styleUrls: ['./breaking-news-strip.component.scss']
+})
+export class BreakingNewsStripComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
